55cfa39a465724f8d01963fa17d86775ad9e6463
Santiago Avila <savila@twilio.com>
2020-05-18 10:57:07 -0500

    [ACCSEC-16905][ACCSEC-16913] - Create SDK project

---
+++ Security/Security.h
@@ -0,0 +1,19 @@
+//
+//  Security.h
+//  Security
+//
+//  Created by Santiago  Avila on 5/15/20.
+//  Copyright © 2020 Twilio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Security.
+FOUNDATION_EXPORT double SecurityVersionNumber;
+
+//! Project version string for Security.
+FOUNDATION_EXPORT const unsigned char SecurityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Security/PublicHeader.h>
+
+
---
+++ TwilioVerifyDemo/TwilioVerifyDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwilioVerifyDemo
+//
+//  Created by Santiago Avila on 5/15/20.
+//  Copyright © 2020 Twilio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
