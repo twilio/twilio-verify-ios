af3b7b6308a5fa539267d563b30bd830fd79987f
Sergio Fierro Perdomo <sfierro@twilio.com>
2020-06-09 10:20:26 -0500

    Verify factor endpoint

--- TwilioVerifyTests/Sources/API/FactorAPIClientTests.swift
+++ TwilioVerifyTests/Sources/API/FactorAPIClientTests.swift
@@ -108,15 +108,121 @@ class FactorAPIClientTests: XCTestCase {
     XCTAssertEqual(String(decoding: networkProvider.urlRequest!.httpBody!, as: UTF8.self), params.asString(),
                    "Body should be \(params.asString()) but was \(networkProvider.urlRequest!.httpBody!)")
   }
+  
+  func testVerifyFactor_withASuccessResponse_shouldCallSuccess() {
+    let successExpectation = expectation(description: "Wait for success response")
+    let expectedResponse = "{\"key\":\"value\"}".data(using: .utf8)!
+    networkProvider.response = Response(data: expectedResponse, headers: [:])
+    let factor = PushFactor(
+      sid: Constants.factorSid,
+      friendlyName: Constants.friendlyName,
+      accountSid: Constants.accountSid,
+      serviceSid: Constants.serviceSid,
+      entityIdentity: Constants.entity,
+      createdAt: Date(),
+      config: Config(credentialSid: Constants.credentialSid))
+    
+    factorAPIClient.verify(factor: factor, authPayload: Constants.authPayload, success: { response in
+      XCTAssertEqual(response.data, expectedResponse, "Response should be \(expectedResponse) but was \(response.data)")
+      successExpectation.fulfill()
+    }) { error in
+      XCTFail()
+      successExpectation.fulfill()
+    }
+    wait(for: [successExpectation], timeout: 5)
+  }
+  
+  func testVerifyFactor_withAnError_shouldCallFailure() {
+    let failureExpectation = expectation(description: "Wait for failure response")
+    let expectedError = TestError.operationFailed
+    networkProvider.error = expectedError
+    let factor = PushFactor(
+      sid: Constants.factorSid,
+      friendlyName: Constants.friendlyName,
+      accountSid: Constants.accountSid,
+      serviceSid: Constants.serviceSid,
+      entityIdentity: Constants.entity,
+      createdAt: Date(),
+      config: Config(credentialSid: Constants.credentialSid))
+    
+    factorAPIClient.verify(factor: factor, authPayload: Constants.authPayload, success: { response in
+      XCTFail()
+      failureExpectation.fulfill()
+    }) { error in
+      XCTAssertEqual(error as! TestError, expectedError)
+      failureExpectation.fulfill()
+    }
+    wait(for: [failureExpectation], timeout: 5)
+  }
+  
+  func testVerifyFactor_withInvalidURL_shouldThrowAndCallFailure() {
+    factorAPIClient = FactorAPIClient(networkProvider: networkProvider, authentication: authentication, baseURL: "%")
+    let failureExpectation = expectation(description: "Wait for failure response")
+    let factor = PushFactor(
+      sid: Constants.factorSid,
+      friendlyName: Constants.friendlyName,
+      accountSid: Constants.accountSid,
+      serviceSid: Constants.serviceSid,
+      entityIdentity: Constants.entity,
+      createdAt: Date(),
+      config: Config(credentialSid: Constants.credentialSid))
+    factorAPIClient.verify(factor: factor, authPayload: Constants.authPayload, success: { response in
+      XCTFail()
+      failureExpectation.fulfill()
+    }) { error in
+      XCTAssertTrue(error is NetworkError)
+      failureExpectation.fulfill()
+    }
+    wait(for: [failureExpectation], timeout: 5)
+  }
+  
+  func testVerifyFactor_shouldMatchExpectedParams() {
+    var expectedParams = Parameters()
+    expectedParams.addAll([Parameter(name: FactorAPIClient.Constants.authPayloadKey, value: Constants.authPayload)])
+    let factor = PushFactor(
+      sid: Constants.factorSid,
+      friendlyName: Constants.friendlyName,
+      accountSid: Constants.accountSid,
+      serviceSid: Constants.serviceSid,
+      entityIdentity: Constants.entity,
+      createdAt: Date(),
+      config: Config(credentialSid: Constants.credentialSid))
+    
+    let expectedURL = "\(Constants.baseURL)\(FactorAPIClient.Constants.verifyFactorURL)"
+      .replacingOccurrences(of: FactorAPIClient.Constants.serviceSidPath, with: factor.serviceSid)
+      .replacingOccurrences(of: FactorAPIClient.Constants.entityPath, with: factor.entityIdentity)
+      .replacingOccurrences(of: FactorAPIClient.Constants.factorSidPath, with: factor.sid)
+    
+    factorAPIClient.verify(factor: factor, authPayload: Constants.authPayload, success: {_ in }, failure: {_ in })
+    
+    XCTAssertEqual(networkProvider.urlRequest!.url!.absoluteString, expectedURL,
+                   "URL should be \(expectedURL) but was \(networkProvider.urlRequest!.url!.absoluteString)")
+    XCTAssertEqual(networkProvider.urlRequest!.httpMethod, HTTPMethod.post.value,
+                   "HTTP method should be \(HTTPMethod.post.value) but was \(networkProvider.urlRequest!.httpMethod!)")
+    XCTAssertEqual(networkProvider.urlRequest!.allHTTPHeaderFields![HTTPHeader.Constant.contentType], MediaType.urlEncoded.value,
+                   "Content type should be \(MediaType.urlEncoded.value) but was \(networkProvider.urlRequest!.allHTTPHeaderFields![HTTPHeader.Constant.contentType]!)")
+    XCTAssertEqual(networkProvider.urlRequest!.allHTTPHeaderFields![HTTPHeader.Constant.acceptType], MediaType.json.value,
+                   "Accept type should be \(MediaType.json.value) but was \(networkProvider.urlRequest!.allHTTPHeaderFields![HTTPHeader.Constant.acceptType]!)")
+    XCTAssertNotNil(networkProvider.urlRequest?.allHTTPHeaderFields![HTTPHeader.Constant.authorization],
+                    "Authorization header should not be nil")
+    XCTAssertNotNil(networkProvider.urlRequest?.allHTTPHeaderFields![HTTPHeader.Constant.userAgent],
+                    "User agent header should not be nil")
+    XCTAssertEqual(String(decoding: networkProvider.urlRequest!.httpBody!, as: UTF8.self), expectedParams.asString(),
+                   "Body should be \(expectedParams.asString()) but was \(networkProvider.urlRequest!.httpBody!)")
+  }
 }
 
 extension FactorAPIClientTests {
   struct Constants {
+    static let factorSid = "sid"
     static let friendlyName = "factor name"
     static let serviceSid = "serviceSid123"
+    static let accountSid = "accountSid123"
     static let entity = "entityIdentity"
+    static let credentialSid = "credentialSid123"
     static let factorType = FactorType.push
     static let jwe = "jwe"
+    static let authPayload = "authPayload"
     static let baseURL = "https://twilio.com/"
   }
 }
