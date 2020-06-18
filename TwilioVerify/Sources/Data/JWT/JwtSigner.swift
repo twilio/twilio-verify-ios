96d175705a382e08820c47eece1875ac504ea984
Yeimi Moreno <ymoreno@twilio.com>
2020-06-18 17:38:31 -0500

    Addressed comments

--- TwilioVerify/Sources/Data/JWT/JwtSigner.swift
+++ TwilioVerify/Sources/Data/JWT/JwtSigner.swift
@@ -52,8 +52,8 @@ private extension JwtSigner {
   func transcodeECSignatureToConcat(_ signature: Data, withOutputLength outputLength: Int) throws -> Data {
     // Parse ASN into just r,s data as defined in:
     // https://tools.ietf.org/html/rfc7518#section-3.4
-    let (asnSig, _) = toASN1Element(data: signature)
-    guard case let ASN1Element.seq(elements: seq) = asnSig,
+    let (asnSignature, _) = toASN1Element(data: signature)
+    guard case let ASN1Element.sequences(elements: seq) = asnSignature,
         seq.count >= 2,
         case let ASN1Element.bytes(data: rData) = seq[0],
         case let ASN1Element.bytes(data: sData) = seq[1]
@@ -61,7 +61,7 @@ private extension JwtSigner {
       throw JwtSignerError.invalidFormat
     }
     // ASN adds 00 bytes in front of negative Int to mark it as positive.
-    // These must be removed to make r,a a valid EC signature
+    // These must be removed to make r,s a valid EC signature
     let trimmedRData: Data
     let trimmedSData: Data
     let rExtra = rData.count - outputLength/2
@@ -83,7 +83,7 @@ private extension JwtSigner {
 private extension JwtSigner {
 
   indirect enum ASN1Element {
-    case seq(elements: [ASN1Element])
+    case sequences(elements: [ASN1Element])
     case integer(int: Int)
     case bytes(data: Data)
     case constructed(tag: Int, elem: ASN1Element)
@@ -92,13 +92,12 @@ private extension JwtSigner {
 
   func toASN1Element(data: Data) -> (ASN1Element, Int) {
     guard data.count >= 2 else {
-      // format error
       return (.unknown, data.count)
     }
 
     switch data[0] {
       case 0x30: // sequence
-        let (length, lengthOfLength) = readLength(data: data.advanced(by: 1))
+        let (length, lengthOfLength) = readLength(from: data.advanced(by: 1))
         var result: [ASN1Element] = []
         var subdata = data.advanced(by: 1 + lengthOfLength)
         var alreadyRead = 0
@@ -109,10 +108,10 @@ private extension JwtSigner {
           subdata = subdata.count > l ? subdata.advanced(by: l) : Data()
           alreadyRead += l
         }
-        return (.seq(elements: result), 1 + lengthOfLength + length)
+        return (.sequences(elements: result), 1 + lengthOfLength + length)
 
       case 0x02: // integer
-        let (length, lengthOfLength) = readLength(data: data.advanced(by: 1))
+        let (length, lengthOfLength) = readLength(from: data.advanced(by: 1))
         if length < 8 {
           var result: Int = 0
           let subdata = data.advanced(by: 1 + lengthOfLength)
@@ -127,18 +126,18 @@ private extension JwtSigner {
 
       case let s where (s & 0xe0) == 0xa0: // constructed
         let tag = Int(s & 0x1f)
-        let (length, lengthOfLength) = readLength(data: data.advanced(by: 1))
+        let (length, lengthOfLength) = readLength(from: data.advanced(by: 1))
         let subdata = data.advanced(by: 1 + lengthOfLength)
         let (e, _) = toASN1Element(data: subdata)
         return (.constructed(tag: tag, elem: e), 1 + lengthOfLength + length)
 
       default: // octet string
-        let (length, lengthOfLength) = readLength(data: data.advanced(by: 1))
+        let (length, lengthOfLength) = readLength(from: data.advanced(by: 1))
         return (.bytes(data: data.subdata(in: (1 + lengthOfLength) ..< (1 + lengthOfLength + length))), 1 + lengthOfLength + length)
     }
   }
 
-  private func readLength(data: Data) -> (Int, Int) {
+  private func readLength(from data: Data) -> (Int, Int) {
     if data[0] & 0x80 == 0x00 { // short form
       return (Int(data[0]), 1)
     } else {
