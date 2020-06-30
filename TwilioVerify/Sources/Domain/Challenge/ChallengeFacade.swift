ebe44ee66748d01a9adefab80928fbddd391d40f
Sergio Fierro Perdomo <sfierro@twilio.com>
2020-06-30 12:24:13 -0500

    ChallengeFacade builder

--- TwilioVerify/Sources/Domain/Challenge/ChallengeFacade.swift
+++ TwilioVerify/Sources/Domain/Challenge/ChallengeFacade.swift
@@ -14,7 +14,20 @@ protocol ChallengeFacadeProtocol {
   func getAll(withPayload challengeListPayload: ChallengeListPayload, success: @escaping (ChallengeList) -> (), failure: @escaping TwilioVerifyErrorBlock)
 }
 
-class ChallengeFacade: ChallengeFacadeProtocol {
+class ChallengeFacade {
+  
+  private let pushChallengeProcessor: PushChallengeProcessor
+  private let factorFacade: FactorFacade
+  private let repository: ChallengeProvider
+  
+  init(pushChallengeProcessor: PushChallengeProcessor, factorFacade: FactorFacade, repository: ChallengeProvider) {
+    self.pushChallengeProcessor = pushChallengeProcessor
+    self.factorFacade = factorFacade
+    self.repository = repository
+  }
+}
+
+extension ChallengeFacade: ChallengeFacadeProtocol {
   func get(withSid sid: String, withFactorSid factorSid: String, success: @escaping ChallengeSuccessBlock, failure: @escaping TwilioVerifyErrorBlock) {
     
   }
@@ -25,5 +38,48 @@ class ChallengeFacade: ChallengeFacadeProtocol {
   
   func getAll(withPayload challengeListPayload: ChallengeListPayload, success: @escaping (ChallengeList) -> (), failure: @escaping TwilioVerifyErrorBlock) {
     
+  }
+}
+
+extension ChallengeFacade {
+  class Builder {
+    
+    private var networkProvider: NetworkProvider!
+    private var jwtGenerator: JwtGenerator!
+    private var factorFacade: FactorFacade!
+    private var url: String!
+    private var authentication: Authentication!
+    
+    func setNetworkProvider(_ networkProvider: NetworkProvider) -> Self {
+      self.networkProvider = networkProvider
+      return self
+    }
+    
+    func setJWTGenerator(_ jwtGenerator: JwtGenerator) -> Self {
+      self.jwtGenerator = jwtGenerator
+      return self
+    }
+    
+    func setFactorFacade(_ factorFacade: FactorFacade) -> Self {
+      self.factorFacade = factorFacade
+      return self
+    }
+    
+    func setURL(_ url: String) -> Self {
+      self.url = url
+      return self
+    }
+    
+    func setAuthentication(_ authentication: Authentication) -> Self {
+      self.authentication = authentication
+      return self
+    }
+    
+    func build() -> ChallengeFacadeProtocol {
+      let challengeAPIClient = ChallengeAPIClient(networkProvider: networkProvider, authentication: authentication, baseURL: url)
+      let repository = ChallengeRepository(apiClient: challengeAPIClient)
+      let pushChallengeProcessor = PushChallengeProcessor(challengeProvider: repository, jwtGenerator: jwtGenerator)
+      return ChallengeFacade(pushChallengeProcessor: pushChallengeProcessor, factorFacade: factorFacade, repository: repository)
+    }
   }
 }
