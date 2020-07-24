63b6b218d1c612accde7cc3382ee5463f866587e
Santiago Avila <savila@twilio.com>
2020-07-24 11:05:57 -0500

    [ACCSEC-18005] - CI

--- AppSizer/AppSizer/Frameworks/TwilioSecurity.framework/Headers/TwilioSecurity.h
+++ AppSizer/AppSizer/Frameworks/TwilioSecurity.framework/Headers/TwilioSecurity.h
@@ -9,10 +9,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for Security.
-FOUNDATION_EXPORT double SecurityVersionNumber;
+FOUNDATION_EXPORT double TwilioSecurityVersionNumber;
 
 //! Project version string for Security.
-FOUNDATION_EXPORT const unsigned char SecurityVersionString[];
+FOUNDATION_EXPORT const unsigned char TwilioSecurityVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Security/PublicHeader.h>
 
