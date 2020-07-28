61c2b1ade9c20d57e754c8ec3454f80f5ffa1a93
Santiago Avila <savila@twilio.com>
2020-07-28 12:14:41 -0500

    [ACCSEC-18005] - CI

--- AppSizer/AppSizer/AppDelegate.swift
+++ AppSizer/AppSizer/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import TwilioVerify
-import TwilioSecurity
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    print("TwilioSecurityVersion: \(KeyManager())")
     print("TwilioVerifyVersion: \(TwilioVerifyBuilder().build())")
     return true
   }
