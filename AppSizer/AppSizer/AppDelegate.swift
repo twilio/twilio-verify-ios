9ca50547d82d8d7aece32c1acb70e5c4c5a1c220
Santiago Avila <sanarroya@gmail.com>
2020-07-30 11:36:18 -0500

    [Housekeeping] - Updated Readme (#67)

--- AppSizer/AppSizer/AppDelegate.swift
+++ AppSizer/AppSizer/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    print("TwilioVerifyVersion: \(TwilioVerifyBuilder().build())")
+    print(TwilioVerifyVersionNumber)
     return true
   }
 }
