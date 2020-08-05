2e8aa2d71b0b65a853c526b0c5f14b0a2198536c
Santiago Avila <savila@twilio.com>
2020-08-05 11:37:12 -0500

    Changed docs folder name and added Script to extract version number

--- Scripts/generate-docs.sh
+++ Scripts/generate-docs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-jazzy \
-  --output docs/0.0.2/ \
-  --theme apple
+marketing_version=($(head -n 1 TwilioVerify/Config/Version.xcconfig))
+version=${marketing_version[@]: -1}
 
+jazzy \
+  --output docs/$version/ \
+  --theme apple
