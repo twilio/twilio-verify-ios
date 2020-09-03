b896c2098eee9b4c5e349216f677df96fb2145aa
Santiago Avila <savila@twilio.com>
2020-09-03 18:12:14 -0500

    [ACCSEC-18583] - Run Integration tests in Firebase Test Lab from CI

--- Scripts/extract-secrets.sh
+++ Scripts/extract-secrets.sh
@@ -10,6 +10,8 @@ mkdir -p ${SECRETS_DIR} || true
 echo $CERT_P12 | base64 -D -o ${SECRETS_DIR}/Certificates.p12
 
 # Extract Provisioning profiles
+echo $PROVISIONING_PROFILES_KEY
+echo $PROVISIONING_PROFILES_IV
 openssl aes-256-cbc -K $PROVISIONING_PROFILES_KEY -iv $PROVISIONING_PROFILES_IV -in Provisioning\ Profiles.tar.enc -out ${SECRETS_DIR}/Provisioning\ Profiles.tar -d
 pushd ${SECRETS_DIR}
   tar -xvf Provisioning\ Profiles.tar 
