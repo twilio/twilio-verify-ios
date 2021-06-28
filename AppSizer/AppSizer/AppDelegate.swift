64483a60fcd149b40d4542918caabc786130ba08
Alejandro Orozco Builes <aorozcobuiles@twilio.com>
2021-06-28 12:26:27 -0500

    [SDK Renaming] - Revert CFBundleVersion

--- AppSizer/AppSizer/AppDelegate.swift
+++ AppSizer/AppSizer/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    if let sdkVersion = Bundle(identifier: "com.twilio.TwilioVerify")?.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String {
+    if let sdkVersion = Bundle(identifier: "com.twilio.TwilioVerify")?.object(forInfoDictionaryKey: "CFBundleVersion") as? String {
         print(sdkVersion)
     }
     return true
